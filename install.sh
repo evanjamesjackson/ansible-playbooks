579a8a52805e9eff4d19a6bddd1dd6a9de4aa6b1
Evan Jackson <evan@jacksonnet.ca>
2025-12-12 21:54:14 -0500

    Fix some Python issues found on new machine

--- install.sh
+++ install.sh
@@ -6,5 +6,5 @@ sudo apt update
 sudo apt install -y software-properties-common python3 python3-pip
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install -y ansible
-sudo pip3 install ansible-dev-tools wcmatch
+sudo pip3 install --break-system-packages --ignore-installed ansible-dev-tools wcmatch
 ansible-galaxy install -r roles/requirements.yml
